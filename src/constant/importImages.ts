212577193c162ca01e6b1ba6001a4c44b3957809
eqypo9 <eqypo@naver.com>
2024-09-04 00:50:06 +0900

    feat: Input Layout

---
+++ src/constant/importImages.ts
@@ -0,0 +1,15 @@
+import IconEyeOn from '../../public/icons/Icon_eye_on.svg';
+import IconEyeOff from '../../public/icons/Icon_eye_off.svg';
+
+export const ICON = {
+  eye: {
+    on: {
+      src: IconEyeOn,
+      alt: '뜬 눈',
+    },
+    off: {
+      src: IconEyeOff,
+      alt: '감은 눈',
+    },
+  },
+}
--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button';
+import { Input } from '@/components/Input';
 import Layout from '@/components/Layout'; 
 
 function Index() {
@@ -9,6 +10,13 @@ function Index() {
         <div className='flex gap-4'>
           <Button>더보기</Button>
           <Button width='20rem'>광장으로 돌아가기</Button>
+        </div>
+      </div>
+
+      <div className='p-4'>
+        <h1 className='text-[2.4rem] font-bold mb-4 text-black'>Input Example</h1>
+        <div className='flex gap-4'>
+          <Input type={'email'} />
         </div>
       </div>
     </Layout>
